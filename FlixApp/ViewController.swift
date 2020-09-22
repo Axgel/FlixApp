f96e890358fca511a932fec2e05708f27b36c9da
Angel Li <liangel421@gmail.com>
2020-09-22 08:28:11 -0400

    Initial Commit

---
+++ FlixApp/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  FlixApp
+//
+//  Created by Angel Li on 9/22/20.
+//  Copyright © 2020 Axgel. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
