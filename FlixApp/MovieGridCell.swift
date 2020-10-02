6ea9cc3fbb0d0ef7cf1f7828120d48ac8a2c32da
Angel Li <liangel421@gmail.com>
2020-10-01 20:45:42 -0400

    Added nav bar

---
+++ FlixApp/MovieGridCell.swift
@@ -0,0 +1,14 @@
+//
+//  MovieGridCell.swift
+//  FlixApp
+//
+//  Created by Angel Li on 10/1/20.
+//  Copyright © 2020 Axgel. All rights reserved.
+//
+
+import UIKit
+
+class MovieGridCell: UICollectionViewCell {
+    
+    @IBOutlet weak var posterView: UIImageView!
+}
